ada8b4f2af8fe9ac90aecaa162fadb859db1163b
Steven <stevenpstansberry@gmail.com>
2024-09-28 00:14:30 -0700

    Added home page details

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,29 @@
+// pages/Home.js
+import React, { useContext } from "react";
+import { DeviceContext } from "../contexts/DeviceContext";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+
 const Home = () => {
+  const { devices } = useContext(DeviceContext);
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Grid container spacing={2}>
+      {devices.map((device) => (
+        <Grid item xs={12} sm={6} md={4} key={device.id}>
+          <Card>
+            <CardContent>
+              <Typography>meow</Typography>
+              <Typography variant="h6">{device.name}</Typography>
+              <Typography variant="body2">Status: {device.status}</Typography>
+              {/* Additional device info */}
+            </CardContent>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
