2321f32267a1a88d3a42ce4dd0a3eaa7189f93e7
Steven <stevenpstansberry@gmail.com>
2024-09-27 23:16:29 -0700

    Refactor App.js to add routing and navigation components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,32 @@
-import "./App.css";
+// App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Home from "./pages/Home";
+import LightingControl from "./pages/LightingControl";
+import AquariumMonitoring from "./pages/AquariumMonitoring";
+import MusicControl from "./pages/MusicControl";
+import AutomationRules from "./pages/AutomationRules";
+import DeviceManagement from "./pages/DeviceManagement";
 
 function App() {
-  return <div className="App">meow</div>;
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <div style={{ marginLeft: 240, padding: 16 }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/lighting-control" element={<LightingControl />} />
+          <Route path="/aquarium-monitoring" element={<AquariumMonitoring />} />
+          <Route path="/music-control" element={<MusicControl />} />
+          <Route path="/automation-rules" element={<AutomationRules />} />
+          <Route path="/device-management" element={<DeviceManagement />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
